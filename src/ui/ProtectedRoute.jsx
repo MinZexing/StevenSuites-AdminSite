f90d6e935d47d9a80b6a60f7f343218b2c1a507a
MinZexing <zexing1908@gmail.com>
2025-03-03 01:42:52 +1100

    complete user login features including page redirect

---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+
+  //1. Load the authenticated user
+  const { isLoading, isAuthenticated } = useUser();
+
+  //3. if no  authenticated, redirect login
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) navigate("/login");
+    },
+    [isAuthenticated, isLoading, navigate]
+  );
+
+  //2. while loading, show spinner
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  //If authenticated, render app
+
+  if (isAuthenticated) return children;
+}
+
+export default ProtectedRoute;
