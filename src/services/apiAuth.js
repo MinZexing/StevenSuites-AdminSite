fb5b69b07154586e5fb7bf9e7721b27ad672e096
MinZexing <zexing1908@gmail.com>
2025-03-03 17:35:02 +1100

    finish email confirmation and route protection features during login

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,5 +1,18 @@
 import supabase from "./supabase";
 
+export async function signup({ fullName, email, password }) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        fullName,
+        avatar: "",
+      },
+    },
+  });
+}
+
 export async function login({ email, password }) {
   let { data, error } = await supabase.auth.signInWithPassword({
     email,
@@ -17,7 +30,6 @@ export async function getCurrentUser() {
   const { data: session } = await supabase.auth.getSession();
   if (!session.session) return null;
   const { data, error } = await supabase.auth.getUser();
-  console.log(data);
   if (error) throw new Error(error.message);
   return data.user;
 }
